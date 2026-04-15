c5aa5b4b59a78be5f116cb6cca949e7cf456cd75
sistemaintegradojuridico <sistemaintegradojuridico@gmail.com>
2026-04-15 09:28:10 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -140,3 +140,7 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
   console.log("Servidor rodando na porta", PORT)
 })
+
+app.get("/", (req,res)=>{
+  res.send("SIJ Backend rodando 🚀")
+})
