55aefe1c9080269e1c22f74f93f338bb17f99e06
sistemaintegradojuridico <sistemaintegradojuridico@gmail.com>
2026-05-04 15:53:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const container = document.getElementById('container');
+const btnRegister = document.getElementById('btnRegister');
+const btnLogin = document.getElementById('btnLogin');
 
-document.getElementById('btnRegister').onclick = () => {
+// alternar telas
+btnRegister.addEventListener('click', () => {
   container.classList.add('active');
-};
+});
 
-document.getElementById('btnLogin').onclick = () => {
+btnLogin.addEventListener('click', () => {
   container.classList.remove('active');
-};
+});
+
+// recuperar senha
+const recuperarSenha = document.getElementById('recuperarSenhaLogin');
+
+if (recuperarSenha) {
+  recuperarSenha.addEventListener('click', () => {
+    alert('Recuperação de senha será implementada em breve');
+  });
+}
