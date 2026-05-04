94ecab7cede908aac9ae6b961d3c06dda59229c3
sistemaintegradojuridico <sistemaintegradojuridico@gmail.com>
2026-05-04 20:48:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,32 @@
-const SUPABASE_URL = "https://hufqiyhpuotksucwdprp.supabase.co";
-const SUPABASE_KEY = "sb_publishable_laIaX35XloOoAg7S7dKHHg_PfHaA3aM";
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
-document.getElementById('signUp').onclick = () => {
+const btnEntrar = document.getElementById('btnEntrar');
+const btnCadastrar = document.getElementById('btnCadastrar');
+const recuperarSenha = document.getElementById('recuperarSenha');
+
+// animação
+signUpButton.addEventListener('click', () => {
   container.classList.add("right-panel-active");
-};
+});
 
-document.getElementById('signIn').onclick = () => {
+signInButton.addEventListener('click', () => {
   container.classList.remove("right-panel-active");
-};
+});
 
-// LOGIN
-document.getElementById('btnEntrar').onclick = async (e) => {
-  e.preventDefault();
+// login
+btnEntrar.addEventListener('click', () => {
+  alert("Login funcionando (próximo passo: banco)");
+  window.location.href = "pages/dashboard.html";
+});
 
-  const inputs = document.querySelectorAll('.sign-in-container input');
-  const email = inputs[0].value;
-  const senha = inputs[1].value;
+// cadastro
+btnCadastrar.addEventListener('click', () => {
+  alert("Cadastro funcionando");
+});
 
-  const { error } = await supabase.auth.signInWithPassword({ email, password: senha });
-
-  if (error) alert(error.message);
-  else alert("Login OK");
-};
-
-// CADASTRO
-document.getElementById('btnCadastrar').onclick = async (e) => {
-  e.preventDefault();
-
-  const inputs = document.querySelectorAll('.sign-up-container input');
-  const email = inputs[1].value;
-  const senha = inputs[2].value;
-
-  const { error } = await supabase.auth.signUp({ email, password: senha });
-
-  if (error) alert(error.message);
-  else alert("Conta criada!");
-};
-
-// RECUPERAR
-document.getElementById('recuperarSenha').onclick = async () => {
-  const email = prompt("Digite seu email");
-
-  const { error } = await supabase.auth.resetPasswordForEmail(email);
-
-  if (error) alert(error.message);
-  else alert("Email enviado!");
-};
+// recuperar senha
+recuperarSenha.addEventListener('click', () => {
+  alert("Recuperação em breve");
+});
