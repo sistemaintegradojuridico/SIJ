07e80285ef2f50c3d5a8b4da923fe40b7d7ece30
sistemaintegradojuridico <sistemaintegradojuridico@gmail.com>
2026-05-04 15:18:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,9 @@
-const API_URL = "https://hufqiyhpuotksucwdprp.supabase.co/rest/v1/clientes";
-const API_KEY = "sb_publishable_laIaX35XloOoAg7S7dKHHg_PfHaA3aM";
+const container = document.getElementById('container');
 
-function mostrar(secao) {
-  document.getElementById("clientes").style.display = "block";
-}
+document.getElementById('btnRegister').onclick = () => {
+  container.classList.add('active');
+};
 
-async function addCliente() {
-  const nome = document.getElementById("nome").value;
-  const email = document.getElementById("email").value;
-  const telefone = document.getElementById("telefone").value;
-
-  const res = await fetch(API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "apikey": API_KEY,
-      "Authorization": `Bearer ${API_KEY}`
-    },
-    body: JSON.stringify({ nome, email, telefone })
-  });
-
-  if (res.ok) {
-    alert("Cliente criado!");
-    listarClientes();
-  } else {
-    alert("Erro ao criar");
-  }
-}
-
-async function listarClientes() {
-  const res = await fetch(API_URL, {
-    headers: {
-      "apikey": API_KEY,
-      "Authorization": `Bearer ${API_KEY}`
-    }
-  });
-
-  const data = await res.json();
-
-  const lista = document.getElementById("listaClientes");
-  lista.innerHTML = "";
-
-  data.forEach(c => {
-    lista.innerHTML += `<p>${c.nome} - ${c.email}</p>`;
-  });
-}
-
-listarClientes();
+document.getElementById('btnLogin').onclick = () => {
+  container.classList.remove('active');
+};
